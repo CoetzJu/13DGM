8723b73fc880e1d07682a2265f6f6e8557fc6941
CoetzJu <CoetzJu@whs.school.nz>
2022-06-07 09:44:13 +1200

    Yay

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,5 @@
+// JavaScript
+// Slideshow
 var slideIndex = 0;
 carousel();
 
@@ -11,4 +13,22 @@ function carousel() {
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+// This is a function for darkmode
+function Darkmode() {
+  var element = document.body;
+  /*const myElement =  document.getElementById("page");
+  myElement.style.color = "red";*/
+  element.classList.toggle("dark-mode");
+}
+
+//experamental JS
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML === "Hello") {
+    x.innerHTML = "Swapped text!";
+  } else {
+    x.innerHTML = "Hello";
+  }
 }
