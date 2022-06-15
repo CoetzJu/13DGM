4c67f4caec24a15770cf20887718b8f287c22a34
CoetzJu <CoetzJu@whs.school.nz>
2022-06-16 08:56:54 +1200

    This is the finished styling there is a bug or two but it's done

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,4 +1,9 @@
 // JavaScript
+
+// You will see a lot of comment out code as this was
+// as they are experimented methods to do functions.
+// Some worked better then others, or they did not work.
+
 // Slideshow
 var slideIndex = 0;
 carousel();
@@ -23,7 +28,8 @@ function Darkmode() {
   element.classList.toggle("dark-mode");
 }
 
-//experamental JS
+// Experamental JS
+// Text swapping
 function myFunction() {
   var x = document.getElementById("myDIV");
   if (x.innerHTML === "Hello") {
@@ -31,4 +37,19 @@ function myFunction() {
   } else {
     x.innerHTML = "Hello";
   }
+}
+
+//For saving the dark-mode between pages
+// https://stackoverflow.com/questions/71198296/keep-dark-or-light-mode-over-different-pages-with-javascript
+checkbox.addEventListener( 'change', function() {
+  localStorage.setItem('dark',this.checked);
+  if(this.checked) {
+       body.classList.add('dark')
+  } else {
+       body.classList.remove('dark')     
+  }
+});
+
+if(localStorage.getItem('dark')) {
+  body.classList.add('dark');
 }
